a008cb35e69f2ddb1f46b8189c922f5893655db4
Mangesh Rananavare <rmangesh1@gmail.com>
2018-03-19 21:28:03 +0100

    Added some comments/documentation of api

--- openbanking-client/src/main/java/eu/dnb/openbanking/client/OpenBankingService.java
+++ openbanking-client/src/main/java/eu/dnb/openbanking/client/OpenBankingService.java
@@ -25,11 +25,24 @@ public class OpenBankingService {
         this.restTemplate = restTemplate;
     }
 
+    /**
+     * Method to get a customer having a particular customer ID
+     *
+     * @param customerId Identifier of the customer
+     * @return Returns the fetched ResponseEntity<Customer>
+     */
     public ResponseEntity<Customer> getCustomer(String customerId) {
         logger.info("Customer id : "+customerId);
         return restTemplate.getForEntity(baseUrl + "/customers/{customerId}", Customer.class, customerId);
     }
 
+    /**
+     * Method to patch the sent in details on the customer identified by the customer ID
+     *
+     * @param customerId Identifier of the customer
+     * @param customerPatch Patch of data to be updated on the customer
+     * @return Returns the ResponseEntity<Customer> as it should look after patching new data
+     */
     public ResponseEntity<Customer> patchCustomer(String customerId, CustomerPatch customerPatch) {
         logger.info("Customer id : "+customerId + ", Customer patch : "+customerPatch);
         HttpEntity<CustomerPatch> customerPatchHttpEntity = new HttpEntity<CustomerPatch>(customerPatch);
